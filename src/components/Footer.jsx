25e0339f2f3e73a93d15339c9930fcf72f58535b
Sharvan Kumar <162790236+kumar-sharvan@users.noreply.github.com>
2025-04-15 11:27:07 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3 w-100 fixed-bottom">
+      <p className="mb-0">
+        Made with <span style={{ color: "red" }}>❤️</span> by{" "}
+        <strong>Sharvan</strong>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
